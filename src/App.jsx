11d17bcf4f29f9bf7f808211ecfab18f4e1f3bb6
Mathias Mutua <triplem706@gmail.com>
2026-03-03 21:08:39 +0300

    Initial Project Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div><h1>  COMP 390: Group Project - FLEETFLOW </h1></div>
+    
+    </>
+  )
+}
+
+export default App
